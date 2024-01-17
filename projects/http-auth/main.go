fabb9d1e10c77d38ab23e6ce50393762bac78f17
nsaimk <nsaimkorkmaz1@gmail.com>
2024-01-17 13:52:10 +0000

    Responds to HTTP get requests with relevant response codes

--- projects/http-auth/main.go
+++ projects/http-auth/main.go
@@ -1,10 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world123"))
+	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("200"))
+	})
+
+	http.HandleFunc("/500", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+	})
+
+	http.HandleFunc("/404", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
 	})
 
 	http.ListenAndServe(":8080", nil)
